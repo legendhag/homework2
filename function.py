6e288039a523c64369aa74c9c33f2802eeacfa6b
hagar <hagarko123@yahoo.com>
2024-06-30 15:09:09 +0300

    version 1 of a simple python function

---
+++ function.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    """
+    This function takes two numbers and returns their sum.
+    """
+    return a + b
+
+# Example usage:
+result = add_numbers(5, 3)
+print("The sum is:", result)
